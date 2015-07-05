0778ca8a76a30a917828263eae67213b39687799
binlalun <david1982>
2015-07-05 11:00:39 +0800

    v1.0

---
+++ ASP_NET_MVC_HOMEWORK/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_NET_MVC_HOMEWORK.Startup))]
+namespace ASP_NET_MVC_HOMEWORK
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
