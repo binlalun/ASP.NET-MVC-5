4370468e561c237a1daf2ac3579d58d900d1e75c
binlalun <binlalun@gmail.com>
2015-07-17 10:53:39 +0800

    V2.0

--- ASP_NET_MVC_HOMEWORK/Models/客戶聯絡人.Partial.cs
+++ ASP_NET_MVC_HOMEWORK/Models/客戶聯絡人.Partial.cs
@@ -3,6 +3,7 @@ namespace ASP_NET_MVC_HOMEWORK.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Data.SqlClient;
 
     [MetadataType(typeof(客戶聯絡人MetaData))]
     public partial class 客戶聯絡人 : IValidatableObject
@@ -11,9 +12,22 @@ namespace ASP_NET_MVC_HOMEWORK.Models
         {
             //var results = new List<ValidationResult>();
 
-            if (this.姓名.Length > 2)
-                yield return new ValidationResult("The last name has too many words!", new[] { "姓名" });
+            //if (this.姓名.Length > 2)
+            //    yield return new ValidationResult("The last name has too many words!", new[] { "姓名" });
 
+            using (var db = new CustomEntities())
+            {
+                
+                
+                //var custcontact = db.客戶聯絡人.SqlQuery("select Id from 客戶聯絡人 where 客戶Id = @客戶Id and Email = @Email",
+                //    new SqlParameter("@客戶Id",this.客戶Id),    
+                //    new SqlParameter("@Email",this.Email)).;
+                //if(custcontact != null && custcontact.Result.Count > 0)
+                    yield return new ValidationResult("該客戶Id的Email重複!", new[] { "Email" });
+
+            }
+
+            
         }
     }
 
@@ -47,6 +61,5 @@ namespace ASP_NET_MVC_HOMEWORK.Models
 
         public virtual 客戶資料 客戶資料 { get; set; }
 
-        
     }
 }
